4e1ec78683eaf4aa64a690ab721ee89312bc9f83
EC2 Default User <ec2-user@ip-172-31-41-36.ec2.internal>
2021-01-02 23:12:11 +0000

    fourth-assignment

---
+++ slide_show.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".slide_show").slick({
+    autoplay: true,
+    autoplaySpeed:2000,
+    arrows: true,
+    prevArrow: '<button class="prev_arrow"> < </button>',
+    nextArrow: '<button class="next_arrow"> > </button>'
+  });
+});
